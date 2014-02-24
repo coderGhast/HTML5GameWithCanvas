4f40e9f34f2a61e6f2739cd09aa9f93713843c39
James <euesdenje@gmail.com>
2014-02-24 23:47:46 +0000

    Mouse position for cross-browser
    Changed the finding of the mouse position to support other and older
    browsers. Thanks to the HTML5 book for this one!

--- public_html/js/first.js
+++ public_html/js/first.js
@@ -1,4 +1,6 @@
 var canvas = document.getElementById('canvas_main');
+// Assigning the mouse/client area listener for the mouse position (utilities.js).
+	mouse = utilities.captureMouse(canvas);
 var context = canvas.getContext('2d');
 
 var canvas_bg = document.getElementById('canvas_city_1');
@@ -9,7 +11,7 @@ shipImageLeft.src = "img/player_ship_l.png";
 var shipImageRight = new Image();
 shipImageRight.src = "img/player_ship_r.png";
 
-var mousePos;
+var mouse;
 var imageY;
 
 var mouseTracking = false;
@@ -29,7 +31,7 @@ var backgroundImage3 = new Image();
 backgroundImage3.src = "img/bg_3.png";
 var bg_x3 = 0;
 	  
-function getMousePos(canvas, evt) {
+function getmouse(canvas, evt) {
 	var rect = canvas.getBoundingClientRect();
 	mouseTracking = true;
 	return {
@@ -43,32 +45,32 @@ function animate(time){
     setTimeout(function() {
 	requestAnimationFrame(animate);
         if(mouseTracking){  
-            if(mousePos.x != null){
-                if(mousePos.x < (canvas.width/2)){
-                    if(mousePos.x < 350 && mousePos.x > 300){
+            if(mouse.x != null){
+                if(mouse.x < (canvas.width/2)){
+                    if(mouse.x < 350 && mouse.x > 300){
                         bg_x1+=4;
                         bg_x2+=2;
                         bg_x3+=1;
-                    } else if(mousePos.x < 300 && mousePos.x > 200) {
+                    } else if(mouse.x < 300 && mouse.x > 200) {
                         bg_x1+=8;
                         bg_x2+=4;
                         bg_x3+=2;
-                    } else if(mousePos.x < 200){
+                    } else if(mouse.x < 200){
                         bg_x1+=16;
                         bg_x2+=8;
                         bg_x3+=4;
                     }
                 }
-                if(mousePos.x > (canvas.width/2)){
-                    if(mousePos.x > 450 && mousePos.x < 500){
+                if(mouse.x > (canvas.width/2)){
+                    if(mouse.x > 450 && mouse.x < 500){
                         bg_x1-=4;
                         bg_x2-=2;
                         bg_x3-=1;
-                    } else if(mousePos.x > 500 && mousePos.x < 600) {
+                    } else if(mouse.x > 500 && mouse.x < 600) {
                         bg_x1-=8;
                         bg_x2-=4;
                         bg_x3-=2;
-                    } else if(mousePos.x > 600){
+                    } else if(mouse.x > 600){
                         bg_x1-=16;
                         bg_x2-=8;
                         bg_x3-=4;
@@ -110,21 +112,6 @@ function loadImages() {
 }
 	
 function paintBackground() {
-/*
-    EDIT: I made a mistake that made me think I was doing it wrong.
-    HOWEVER.. Still maybe better practice to do this on other canvases.
-
-    THIS MUST BE DONE USING DIFFERENT CANVASES!!
-    Currently, each time another BG is drawn (so, let's
-    say we reach the end and want to paint BG_3 again),
-    it will be painting over our BG_2 and BG_1 if they have not yet been finished.
-    Only other way to solve this would be through the use of images
-    all of the same side when painted, perhaps.. Idk.. Yes.. 
-
-    So, do them in different canvases. The drawing order/time
-    can be the same, but it mainly affects the layering. Using
-    z-index should make it work just fine. Try it now!
-*/
     context_bg.drawImage(backgroundImage3, bg_x3-1000, 0);
     context_bg.drawImage(backgroundImage3, bg_x3+1000, 0);
 	context_bg.drawImage(backgroundImage3, bg_x3, 0);
@@ -140,8 +127,8 @@ shipImageLeft.onload= function(){
 
 };
 function paintShipLeft() {
-	if(mousePos.y < 475 && mousePos.y > 25){
-		imageY = mousePos.y - 25;	
+	if(mouse.y < 475 && mouse.y > 25){
+		imageY = mouse.y - 25;	
 	}
 	context.drawImage(shipImageLeft, (canvas.width/2) - 37, imageY, 75, 50);
 }
@@ -149,15 +136,15 @@ shipImageRight.onload = function(){
 
 };
 function paintShipRight() {
-	if(mousePos.y < 475 && mousePos.y > 25){
-		imageY = mousePos.y - 25;
+	if(mouse.y < 475 && mouse.y > 25){
+		imageY = mouse.y - 25;
 	}
 	context.drawImage(shipImageRight, (canvas.width/2) - 37, imageY, 75, 50);
 }
 function shipPaint() {
 	clearCanvas();
 	paintBackground();
-	if(mousePos.x < 400){
+	if(mouse.x < 400){
 		paintShipLeft();
 	}else{
 		paintShipRight();
@@ -165,6 +152,6 @@ function shipPaint() {
 }	
 
 canvas.addEventListener('mousemove', function(evt) {
-	mousePos = getMousePos(canvas, evt);
+	mouse = getmouse(canvas, evt);
 		shipPaint();
 }, false);
--- public_html/js/utilities.js
+++ public_html/js/utilities.js
@@ -1,1 +1,53 @@
+/*
+	Many utility functons here have been learned from the book:
+	Foundation HTML5 Animation with JavaScript, by Billy
+	Lamberta and Keith Peters.
+	
+	They help support cross-browser compatibility and older browsers
+	that may not be up to the current standard.
+
+*/
+
 var utilities = {}
+/**
+	Support animating for the game loop in browsers that may not yet
+	have implemented 'requestAnimationFrame' for cross-browser compatibility.
+*/
+if(!window.requestAnimationFrame){
+	window.requestAnimationFrame = (window.webkitRequestAnimationFrame ||
+									window.mozRequestAnimationFrame ||
+									window.oRequestAnimationFrame ||
+									window.msRequestAnimationFrame ||
+									function (callback) {
+										return window.setTimeout(callback, 1000/60);
+									});
+}
+	
+/**
+	Support for getting the position of the mouse upon an element
+	passed as a parameter. Not all browsers support 'mousePos'
+	with the mouse listener.
+*/	
+utilities.captureMouse = function (element) {
+	var mouse = {x: 0, y:0};
+	
+	element.addEventListener('mousemove', function (event) {
+		var x, y;
+		if (event.pageX || event.pageY) {
+			x = event.pageX;
+			y = event.pageY;
+		} else {
+			x = event.clientX + document.body.scrollLeft +
+				document.documentElement.scrollLeft;
+			y = event.clientY + document.body.scrollTop +
+				document.documentElement.scrollTop;
+			}
+			x -= element.offsetLeft;
+			y -= element.offsetTop;
+			
+			mouse.x = x;
+			mouse.y = y;
+			}, false);
+			
+			return mouse;
+		};
