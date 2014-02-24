9a4af435d54629c61518e632b6dd1406c354000f
James <euesdenje@gmail.com>
2014-02-24 17:02:22 +0000

    Variable speed, looped BG

--- public_html/js/first.js
+++ public_html/js/first.js
@@ -1,5 +1,9 @@
-var canvas = document.getElementById('canvas_object');
+var canvas = document.getElementById('canvas_main');
 var context = canvas.getContext('2d');
+
+var canvas_bg = document.getElementById('canvas_city_1');
+var context_bg = canvas_bg.getContext('2d');
+
 var	shipImageLeft = new Image();
 shipImageLeft.src = "img/player_ship_l.png";
 var shipImageRight = new Image();
@@ -9,8 +13,9 @@ var mousePos;
 var imageY;
 
 var mouseTracking = false;
+var fps = 66;
 
-var bg_offset;
+var bg_offset=1000;
 
 var backgroundImage1 = new Image();
 backgroundImage1.src = "img/bg_1.png";
@@ -35,34 +40,66 @@ function getMousePos(canvas, evt) {
 
 loadImages();
 function animate(time){
-	if(mouseTracking){  
-		if(mousePos.x != null){
-			if(mousePos.x < 370){
-				if(bg_x1 < 300){ //Restrict movement to rectangle
-					bg_x1+=10;
-					bg_x2+=5;
-					bg_x3+=2;
-				}
-			}
-			if(mousePos.x > 430) {
-				if(bg_x1 > -7500){ //Restrict movement to rectangle
-					bg_x1-=10;
-					bg_x2-=5;
-					bg_x3-=2;
-				}
-			}
-		}
-		
-		paintBackground();
-		shipPaint();
-	}
+    setTimeout(function() {
 	requestAnimationFrame(animate);
+        if(mouseTracking){  
+            if(mousePos.x != null){
+                if(mousePos.x < 400){
+                    if(mousePos.x < 350 && mousePos.x > 300){
+                        bg_x1+=4;
+                        bg_x2+=2;
+                        bg_x3+=1;
+                    } else if(mousePos.x < 300 && mousePos.x > 200) {
+                        bg_x1+=8;
+                        bg_x2+=4;
+                        bg_x3+=2;
+                    } else if(mousePos.x < 200){
+                        bg_x1+=16;
+                        bg_x2+=8;
+                        bg_x3+=4;
+                    }
+                }
+                if(mousePos.x > 400){
+                    if(mousePos.x > 450 && mousePos.x < 500){
+                        bg_x1-=4;
+                        bg_x2-=2;
+                        bg_x3-=1;
+                    } else if(mousePos.x > 500 && mousePos.x < 600) {
+                        bg_x1-=8;
+                        bg_x2-=4;
+                        bg_x3-=2;
+                    } else if(mousePos.x > 600){
+                        bg_x1-=16;
+                        bg_x2-=8;
+                        bg_x3-=4;
+                    }
+                }
+                /*
+                    For checking bounds of 
+                    background and repeating it!
+                */
+                if(bg_x1 > 0){
+                    bg_x1 = -4000;
+                    bg_x2 = -2000;
+                    bg_x3 = -1000;
+                }
+                if(bg_x1 < (0 - 4000)){
+                    bg_x1 = 0;
+                    bg_x2 = 0;
+                    bg_x3 = 0;
+                }
+            }
+        paintBackground();
+        shipPaint();
+        }
+    }, 1000/ fps);
 };
 
 requestAnimationFrame(animate);
 
 function clearCanvas() {
 	context.clearRect(0, 0, canvas.width, canvas.height);
+    context_bg.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function loadImages() {  
@@ -73,16 +110,30 @@ function loadImages() {
 }
 	
 function paintBackground() {
-   context.translate(-bg_offset, 0);
+/*
+    EDIT: I made a mistake that made me think I was doing it wrong.
+    HOWEVER.. Still maybe better practice to do this on other canvases.
 
-	context.drawImage(backgroundImage3, bg_x3, 0);
-	context.drawImage(backgroundImage3, bg_x3 + 1000, 0);
-	context.drawImage(backgroundImage2, bg_x2, 0);
-	context.drawImage(backgroundImage2, bg_x2 + 2000, 0);
-	context.drawImage(backgroundImage1, bg_x1, 0);
-	context.drawImage(backgroundImage1, bg_x1 + 4000, 0);
+    THIS MUST BE DONE USING DIFFERENT CANVASES!!
+    Currently, each time another BG is drawn (so, let's
+    say we reach the end and want to paint BG_3 again),
+    it will be painting over our BG_2 and BG_1 if they have not yet been finished.
+    Only other way to solve this would be through the use of images
+    all of the same side when painted, perhaps.. Idk.. Yes.. 
 
-   //context.translate(bg_offset, 0);
+    So, do them in different canvases. The drawing order/time
+    can be the same, but it mainly affects the layering. Using
+    z-index should make it work just fine. Try it now!
+*/
+    context_bg.drawImage(backgroundImage3, bg_x3-1000, 0);
+    context_bg.drawImage(backgroundImage3, bg_x3+1000, 0);
+	context_bg.drawImage(backgroundImage3, bg_x3, 0);
+    context_bg.drawImage(backgroundImage2, bg_x2-2000, 0);
+    context_bg.drawImage(backgroundImage2, bg_x2+2000, 0);
+	context_bg.drawImage(backgroundImage2, bg_x2, 0);
+    context_bg.drawImage(backgroundImage1, bg_x1-4000, 0);
+    context_bg.drawImage(backgroundImage1, bg_x1+4000, 0);
+	context_bg.drawImage(backgroundImage1, bg_x1, 0);
 }
 
 shipImageLeft.onload= function(){
