d353b982751ef1db815f76870b53f2584d9f0a09
James <euesdenje@gmail.com>
2014-03-27 04:55:22 +0000

    Assignment brief and small changes
    Uploading the assignment brief (Game and report), and a couple of
    changes to the main game js file.

--- public_html/js/cat.js
+++ public_html/js/cat.js
@@ -18,7 +18,9 @@ right_paw_image.src = "img/right_paw.png";
 var left_paw_grabbing = false;
 var right_paw_grabbing = false;
 
-var fps = 33;
+var fps = 11;
+var left_movement_step = 0;
+var right_movement_step = 0;
 
 
 paintBackground();
@@ -53,9 +55,11 @@ requestAnimationFrame(animate);
 function left_paw_move() {
     if(!left_paw_grabbing){
         left_paw_grabbing = true;
+        right_movement_step = 0;
         right_paw_grabbing = false;
     } else {
         left_paw_grabbing = false;
+        left_movement_step = 0;
         right_paw_grabbing = false;
     }
 }
@@ -63,31 +67,33 @@ function left_paw_move() {
 function right_paw_move() {
     if(!right_paw_grabbing){
         right_paw_grabbing = true;
+        left_movement_step = 0;
         left_paw_grabbing = false;
     } else {
         right_paw_grabbing = false;
+        right_movement_step = 0;
         left_paw_grabbing = false;
     }
 }
 
 
 function paint_left_paw_grab(){
+
+    var rotation = 10 * left_movement_step;
+    var width_position = left_movement_step * 10;
+    var height_position = left_movement_step * 0.5;
     clearCanvas();
     paintBackground();
 
     // save the unrotated context of the canvas so we can restore it later
     // the alternative is to untranslate & unrotate after drawing
     context.save();
-
-    // move to the center of the canvas
-    context.translate((canvas_width - left_paw_image.width) / 2,left_paw_image.height);
-
-    // rotate the canvas to the specified degrees
-    context.rotate(50*Math.PI/180);
-
-    // draw the image
-    // since the context is rotated, the image will be rotated also
-    context.drawImage(left_paw_image,0,-left_paw_image.height * 1.4);
+    context.translate((canvas_width - left_paw_image.width) / width_position, left_paw_image.height * height_position);
+    context.rotate(rotation *Math.PI/180);
+    context.drawImage(left_paw_image,0, -left_paw_image.height);
+    if(left_movement_step <= 4){
+        left_movement_step++;
+    }   
 
     // we’re done with the rotating so restore the unrotated context
     context.restore();
